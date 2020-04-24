f06a14247492c005b88de3bb7992945067ef51db
Junaid Bhura <root@junaid.dev>
2020-04-25 07:52:41 +1000

    Update Image Control

--- assets/src/components/image-control/index.js
+++ assets/src/components/image-control/index.js
@@ -1,7 +1,6 @@
 import './editor.scss';
 
 import wp from 'wp';
-import React from 'react';
 import has from 'lodash/has';
 import isObject from 'lodash/isObject';
 import classnames from 'classnames';
@@ -10,6 +9,11 @@ const {
 	withSelect,
 	withDispatch,
 } = wp.data;
+
+const {
+	useState,
+	useEffect,
+} = wp.element;
 
 const { __ } = wp.i18n;
 
@@ -24,63 +28,52 @@ const {
 	MediaUpload,
 } = wp.editor;
 
-class ImageControl extends React.Component {
-	constructor( props ) {
-		super( props );
+function ImageControl( { label, help, value, size, selectLabel = __( 'Select image' ), removeLabel = __( 'Remove image' ), onChange, onSetMedia, selectedMedia } ) {
+	const [ id, setId ] = useState( null );
+	const [ controlValue, setControlValue ] = useState( null );
 
-		this.state = {
-			id: null,
-			value: null,
-			media: null,
-		};
-	}
-
-	componentDidMount() {
-		if ( this.props.value ) {
-			if ( ! isObject( this.props.value ) ) {
-				this.setState( {
-					id: this.props.value,
-				} );
+	useEffect(
+		() => {
+			if ( ! isObject( value ) ) {
+				setId( value );
+				setControlValue( null );
 			} else {
-				this.setState( {
-					id: this.props.value.id,
-					value: this.props.value,
-					media: null,
-				} );
+				setId( value.id );
+				setControlValue( value );
 			}
-		}
-	}
+		},
+		[ value ]
+	);
 
-	componentDidUpdate( prevProps ) {
-		const { media } = this.props;
-		if ( prevProps.media !== media && null !== media ) {
-			this.setState( {
-				id: media.id,
-				value: this.getImageDetails( media ),
-				media,
-			} );
-		}
-	}
+	useEffect(
+		() => {
+			if ( isObject( selectedMedia ) ) {
+				setId( value.id );
+				setControlValue( value );
+			}
+		},
+		[ selectedMedia ]
+	);
 
-	getImageDetails( media ) {
+	const getImageDetails = ( media ) => {
 		if ( ! media ) {
 			return {};
 		}
 
-		let size = 'full';
+		let mediaSize = 'full';
 		let src, width, height;
 
-		if ( this.props.size ) {
-			size = this.props.size;
+		if ( size ) {
+			mediaSize = size;
 		}
 
 		if ( has( media, 'sizes' ) ) {
-			if ( ! has( media.sizes, size ) ) {
-				size = 'full';
+			if ( ! has( media.sizes, mediaSize ) ) {
+				mediaSize = 'full';
 			}
-			width = media.sizes[ size ].width;
-			height = media.sizes[ size ].height;
-			src = media.sizes[ size ].url;
+			width = media.sizes[ mediaSize ].width;
+			height = media.sizes[ mediaSize ].height;
+			src = media.sizes[ mediaSize ].url;
 		}
 
 		return {
@@ -91,107 +84,88 @@ class ImageControl extends React.Component {
 			alt: media.alt,
 			caption: media.caption,
 			title: media.title,
-			size,
+			size: mediaSize,
 		};
-	}
+	};
 
-	render() {
-		const { id, value } = this.state;
-		const { label, help } = this.props;
-		let { selectLabel, removeLabel } = this.props;
+	const onSelectImage = ( media ) => {
+		const image = getImageDetails( media );
 
-		if ( ! selectLabel ) {
-			selectLabel = __( 'Select image' );
+		setId( null );
+		setControlValue( image );
+
+		onSetMedia( media );
+
+		if ( onChange ) {
+			onChange( image, media );
 		}
-		if ( ! removeLabel ) {
-			removeLabel = __( 'Remove image' );
+	};
+
+	const onRemoveImage = () => {
+		setId( null );
+		setControlValue( null );
+
+		if ( onChange ) {
+			onChange( null, null );
 		}
+	};
 
-		const onSelectImage = ( media ) => {
-			const value = this.getImageDetails( media );
-
-			this.setState( {
-				id: media.id,
-				value,
-				media,
-			} );
-
-			this.props.onSetMedia( media );
-
-			if ( this.props.onChange ) {
-				this.props.onChange( value, media );
+	return (
+		<BaseControl
+			label={ label }
+			help={ help }
+			className={ classnames( 'gumponents-image-control', {
+				'gumponents-image-control--selected': null !== controlValue,
+			} ) }
+		>
+			{ id &&
+				<div className="gumponents-image-control__preview">
+					{ ! controlValue &&
+						<Spinner />
+					}
+					{ controlValue &&
+						<MediaUpload
+							title={ selectLabel }
+							onSelect={ ( media ) => onSelectImage( media ) }
+							type="image"
+							value={ id }
+							render={ ( { open } ) => (
+								<Button className="gumponents-image-control__preview" onClick={ open }>
+									<ResponsiveWrapper
+										naturalWidth={ controlValue.width }
+										naturalHeight={ controlValue.height }
+									>
+										<img src={ controlValue.src } alt="" />
+									</ResponsiveWrapper>
+								</Button>
+							) }
+						/>
+					}
+				</div>
 			}
-		};
-
-		const onRemoveImage = () => {
-			this.setState( {
-				id: null,
-				value: null,
-				media: null,
-			} );
-
-			if ( this.props.onChange ) {
-				this.props.onChange( null, null );
+			{ controlValue &&
+				<Button onClick={ onRemoveImage } isLink isDestructive>
+					{ removeLabel }
+				</Button>
 			}
-		};
-
-		return (
-			<BaseControl
-				label={ label }
-				help={ help }
-				className={ classnames( 'gumponents-image-control', {
-					'gumponents-image-control--selected': null !== value,
-				} ) }
-			>
-				{ id &&
-					<div className="gumponents-image-control__preview">
-						{ ! value &&
-							<Spinner />
-						}
-						{ value &&
-							<MediaUpload
-								title={ selectLabel }
-								onSelect={ ( media ) => onSelectImage( media ) }
-								type="image"
-								value={ id }
-								render={ ( { open } ) => (
-									<Button className="gumponents-image-control__preview" onClick={ open }>
-										<ResponsiveWrapper
-											naturalWidth={ value.width }
-											naturalHeight={ value.height }
-										>
-											<img src={ value.src } alt="" />
-										</ResponsiveWrapper>
-									</Button>
-								) }
-							/>
-						}
-					</div>
-				}
-				{ value &&
-					<Button onClick={ onRemoveImage } isLink isDestructive>
-						{ removeLabel }
-					</Button>
-				}
-				{ ! value &&
-					<MediaUpload
-						title={ selectLabel }
-						onSelect={ ( media ) => onSelectImage( media ) }
-						type="image"
-						render={ ( { open } ) => (
-							<Button
-								isDefault
-								className="gumponents-image-control__select"
-								onClick={ open }
-							>
-								{ selectLabel }
-							</Button>
-						) }
-					/>
-				}
-			</BaseControl>
-		);
-	}
+			{ ! controlValue &&
+				<MediaUpload
+					title={ selectLabel }
+					onSelect={ ( media ) => onSelectImage( media ) }
+					type="image"
+					render={ ( { open } ) => (
+						<Button
+							isDefault
+							className="gumponents-image-control__select"
+							onClick={ open }
+						>
+							{ selectLabel }
+						</Button>
+					) }
+				/>
+			}
+		</BaseControl>
+	);
 }
 
 export default withSelect( ( select, ownProps ) => {
@@ -199,7 +173,7 @@ export default withSelect( ( select, ownProps ) => {
 	const { value } = ownProps;
 
 	return {
-		media: value ? getMedia( value ) : null,
+		selectedMedia: value ? getMedia( value ) : null,
 	};
 } )( withDispatch( ( dispatch ) => {
 	return {
