ce6f661dea233e850f77e51b3345395ce0d7cf1b
Junaid Bhura <root@junaid.dev>
2020-04-25 17:18:30 +1000

    Update SelectImage

--- assets/src/components/select-image/index.js
+++ assets/src/components/select-image/index.js
@@ -4,160 +4,81 @@ import wp from 'wp';
 import React from 'react';
 import classnames from 'classnames';
 import isEmpty from 'lodash/isEmpty';
-import has from 'lodash/has';
+import { getImageDetails } from '../image-control';
 
 import ImageContainer from './image-container';
 
 const { __ } = wp.i18n;
-
 const {
 	MediaUpload,
 } = wp.blockEditor;
-
 const {
 	Modal,
 	TextControl,
 	TextareaControl,
 } = wp.components;
+const { useState } = wp.element;
 
-class SelectImage extends React.Component {
-	constructor( props ) {
-		super( props );
+export default function SelectImage( { image, size = 'full', className, placeholder, showCaption, onChange } ) {
+	const [ modalOpen, setModalOpen ] = useState( false );
 
-		this.state = {
-			image: {},
-			media: {},
-			modalOpen: false,
-		};
-	}
-
-	componentDidMount() {
-		if ( this.props.image ) {
-			this.setState( {
-				image: this.props.image,
-			} );
+	const imageSelected = ( media ) => {
+		if ( onChange ) {
+			if ( ! media ) {
+				onChange( {} );
+			} else {
+				onChange( getImageDetails( media, size ) );
+			}
 		}
-	}
+	};
 
-	componentDidUpdate( prevProps, prevState ) {
-		if ( prevState.image !== this.state.image && this.props.onChange ) {
-			this.props.onChange( this.state.image, this.state.media );
-		}
-		if ( prevState.media !== this.state.media && this.props.onMedia ) {
-			this.props.onMedia( this.state.media );
-		}
-	}
-
-	updateImage( newImg ) {
-		this.setState( { image: newImg } );
-		if ( this.props.onChange ) {
-			this.props.onChange( newImg, this.state.media );
-		}
-	}
-
-	render() {
-		const { image, modalOpen } = this.state;
-
-		const {
+	return (
+		<div className={ classnames(
+			'gumponents-select-image',
 			className,
-			placeholder,
-			showCaption,
-		} = this.props;
-
-		let {
-			size,
-		} = this.props;
-
-		if ( 'undefined' === typeof size ) {
-			size = 'thumbnail';
-		}
-
-		return (
-			<div className={ classnames(
-				'gumponents-select-image',
-				className,
-				! isEmpty( image ) ? 'gumponents-select-image--selected' : null,
-				! placeholder ? 'gumponents-select-image--no-placeholder' : null
-			) }>
-				<MediaUpload
-					onSelect={ ( media ) => {
-						if ( ! media ) {
-							this.setState( {
-								image: {},
-								media: {},
-							} );
-							return;
-						}
-
-						if ( ! has( media.sizes, size ) ) {
-							size = 'full';
-						}
-
-						this.setState( {
-							image: {
-								id: media.id,
-								src: media.sizes[ size ].url,
-								width: media.sizes[ size ].width,
-								height: media.sizes[ size ].height,
-								alt: media.alt,
-								caption: media.caption,
-								title: media.title,
-								size,
-							},
-							media,
-						} );
-					} }
-					type="image"
-					value={ image.id }
-					render={ ( { open } ) => (
-						<ImageContainer
-							placeholder={ placeholder }
-							image={ image }
-							open={ open }
-							showCaption={ showCaption }
-							onRemove={ () => this.setState( { image: {}, media: {} } ) }
-							onEdit={ () => this.setState( { modalOpen: true } ) }
-							onCaptionEdit={ ( newImg ) => this.updateImage( newImg ) }
-						/>
-					) }
-				/>
-				{ modalOpen &&
-					<Modal
-						title={ __( 'Edit Image', 'gumponents' ) }
-						className="gumponents-select-image__modal"
-						onRequestClose={ () => this.setState( { modalOpen: false } ) }>
-						<TextControl
-							label={ __( 'Alt Text', 'gumponents' ) }
-							value={ image.alt }
-							onChange={ ( alt ) => {
-								let newImg = image;
-								newImg.alt = alt;
-								this.updateImage( newImg );
-							} }
-						/>
-						<TextareaControl
-							label={ __( 'Caption', 'gumponents' ) }
-							value={ image.caption }
-							onChange={ ( caption ) => {
-								let newImg = image;
-								newImg.caption = caption;
-								this.updateImage( newImg );
-							} }
-						/>
-						<TextControl
-							label={ __( 'Title', 'gumponents' ) }
-							value={ image.title }
-							onChange={ ( title ) => {
-								let newImg = image;
-								newImg.title = title;
-								this.updateImage( newImg );
-							} }
-						/>
-					</Modal>
-				}
-			</div>
-		);
-	}
+			{
+				'gumponents-select-image--selected': ! isEmpty( image ),
+				'gumponents-select-image--no-placeholder': ! placeholder,
+			}
+		) }>
+			<MediaUpload
+				onSelect={ imageSelected }
+				allowedTypes={ [ 'image' ] }
+				value={ image ? image.id : null }
+				render={ ( { open } ) => (
+					<ImageContainer
+						placeholder={ placeholder }
+						image={ image }
+						open={ open }
+						showCaption={ showCaption }
+						onRemove={ () => onChange( {} ) }
+						onEdit={ () => setModalOpen( true ) }
+						onCaptionEdit={ ( newImg ) => onChange( newImg ) }
+					/>
+				) }
+			/>
+			{ modalOpen &&
+				<Modal
+					title={ __( 'Edit Image', 'gumponents' ) }
+					className="gumponents-select-image__modal"
+					onRequestClose={ () => setModalOpen( false ) }>
+					<TextControl
+						label={ __( 'Alt Text', 'gumponents' ) }
+						value={ image.alt }
+						onChange={ ( alt ) => onChange( { ...image, alt } ) }
+					/>
+					<TextareaControl
+						label={ __( 'Caption', 'gumponents' ) }
+						value={ image.caption }
+						onChange={ ( caption ) => onChange( { ...image, caption } ) }
+					/>
+					<TextControl
+						label={ __( 'Title', 'gumponents' ) }
+						value={ image.title }
+						onChange={ ( title ) => onChange( { ...image, title } ) }
+					/>
+				</Modal>
+			}
+		</div>
+	);
 }
-
-export default SelectImage;
