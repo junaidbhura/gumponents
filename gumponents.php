10146f647c84988e6a234bb262e31a58a3010d4d
Junaid Bhura <root@junaid.dev>
2022-08-04 18:04:42 +1000

    bump version number

--- gumponents.php
+++ gumponents.php
@@ -4,14 +4,14 @@
  * Description: Essential Gutenberg components for WordPress.
  * Author: Junaid Bhura
  * Author URI: https://junaid.dev
- * Version: 0.8.0
+ * Version: 0.9.0
  *
  * @package gumponents
  */
 
 namespace JB\Gumponents;
 
-define( 'GUMPONENTS_VERSION', '0.8.0' );
+define( 'GUMPONENTS_VERSION', '0.9.0' );
 
 require_once __DIR__ . '/inc/autoload.php';
 require_once __DIR__ . '/inc/namespace.php';
